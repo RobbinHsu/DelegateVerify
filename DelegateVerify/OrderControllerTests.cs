9f23da1b4d29b74d95f6dfa3420bf668b51b8d2b
Robbin.Hsu <robbin.hsu@collaborate.tw>
2020-06-18 17:12:15 +0800

    Refactor Test Case

--- DelegateVerify/OrderControllerTests.cs
+++ DelegateVerify/OrderControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -9,6 +10,7 @@ namespace DelegateVerify
     {
         private ILog _log;
         private IOrderModel _model;
+        private Order _order;
         private OrderController _orderController;
 
         [SetUp]
@@ -22,36 +24,72 @@ namespace DelegateVerify
         [Test]
         public void exist_order_should_update()
         {
-            var order = new Order {Id = 91, Amount = 100};
-            _model.Save(order, Arg.Any<Action<Order>>(), Arg.Invoke(order));
-            _orderController.Save(order);
-
-            _log.Received(1).Write(Arg.Is<string>(x => x.Contains("update")));
+            CreateOrder();
+            WhenUpdateMessage();
+            ShouldReceivedMessageContains("update");
         }
 
         [Test]
         public void no_exist_order_should_insert()
         {
-            var order = new Order {Id = 91, Amount = 100};
-            _model.Save(order, Arg.Any<Action<Order>>(), Arg.Any<Action<Order>>());
-            _orderController.Save(order);
-
-            _log.DidNotReceiveWithAnyArgs();
+            CreateOrder();
+            WhenNoMessageInsert();
+            ShouldNotReceiveMessage();
         }
 
         [Test]
         public void test_delete_adult_orders()
         {
-            var order = new Order {Id = 91, Amount = 100, Customer = new Customer() {Age = 30}};
+            CreateOrderWhenCustomerAgeMoreThen(30);
+            var modelDeletePredicate = WhenDeleteAdultOrder();
+            ShouldDeleteAdultOrder(modelDeletePredicate);
+        }
 
+        private void ShouldDeleteAdultOrder(Func<Order, bool> modelDeletePredicate)
+        {
+            Assert.IsTrue(modelDeletePredicate(_order));
+        }
+
+        private Func<Order, bool> WhenDeleteAdultOrder()
+        {
             Func<Order, bool> modelDeletePredicate = null;
-
             _model.When(m => m.Delete(Arg.Any<Func<Order, bool>>()))
                 .Do(m => modelDeletePredicate = m[0] as Func<Order, bool>);
 
             _orderController.DeleteAdultOrders();
+            return modelDeletePredicate;
+        }
 
-            Assert.IsTrue(modelDeletePredicate(order));
+        private void WhenNoMessageInsert()
+        {
+            _model.Save(_order, Arg.Any<Action<Order>>(), Arg.Any<Action<Order>>());
+            _orderController.Save(_order);
+        }
+
+        private void ShouldNotReceiveMessage()
+        {
+            _log.DidNotReceive().Write(Arg.Any<string>());
+        }
+
+        private void WhenUpdateMessage()
+        {
+            _model.Save(_order, Arg.Any<Action<Order>>(), Arg.Invoke(_order));
+            _orderController.Save(_order);
+        }
+
+        private void ShouldReceivedMessageContains(string update)
+        {
+            _log.Received(1).Write(Arg.Is<string>(x => x.Contains(update)));
+        }
+
+        private void CreateOrderWhenCustomerAgeMoreThen(int age)
+        {
+            _order = new Order {Id = 91, Amount = 100, Customer = new Customer() {Age = age}};
+        }
+
+        private void CreateOrder()
+        {
+            _order = new Order {Id = 91, Amount = 100};
         }
     }
 }
